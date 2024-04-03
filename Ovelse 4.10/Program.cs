aea9c0c51880c377b1ab2b0e5d433a1dfb75d679
Christoffer Olsen <chris70944@gmail.com>
2024-04-03 10:09:47 +0200

    Done

--- Ovelse 4.10/Program.cs
+++ Ovelse 4.10/Program.cs
@@ -1,5 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Screeningen skal anbefales for mænd over 50. Kvinder skal have at vide, at de ikke er i risikogruppen,
-og mænd under 50 skal have at vide, at de bør blive screenet når de er over 50.
 
+/*creeningen skal anbefales for mænd over 50. Kvinder skal have at vide, at de ikke er i risikogruppen,
+og mænd under 50 skal have at vide, at de bør blive screenet når de er over 50.*/
+
+Console.WriteLine("Hvad er dit køn (mand/kvinde)");
+var koen = Console.ReadLine();
+
+Console.WriteLine("Hvor gammel er du?");
+int alder = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear();
+if (koen == "kvinde")
+{
+    Console.WriteLine("Du er ikke i risikogruppen.");
+} else
+{
+    if (alder > 50)
+    {
+        Console.WriteLine("Du bør tage en screeningen.");
+    } else
+    {
+        Console.WriteLine("Du bør tage en sceenet, når du er 50 år gammel.");
+    }
+    
+}
