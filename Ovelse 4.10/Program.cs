8ecb47c564e50be1d108e1e10623a98ffbe2ee09
Christoffer Olsen <chris70944@gmail.com>
2024-04-03 09:41:28 +0200

    Add project files.

---
+++ Ovelse 4.10/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Screeningen skal anbefales for mænd over 50. Kvinder skal have at vide, at de ikke er i risikogruppen,
+og mænd under 50 skal have at vide, at de bør blive screenet når de er over 50.
+
